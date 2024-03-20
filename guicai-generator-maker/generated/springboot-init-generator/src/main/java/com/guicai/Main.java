06bfde2f968013f19aa319b97945df55c6495dad
guicaizhici <969774823@qq.com>
2024-03-21 00:45:22 +0800

    测试文件生成

---
+++ guicai-generator-maker/generated/springboot-init-generator/src/main/java/com/guicai/Main.java
@@ -0,0 +1,11 @@
+package com.guicai;
+
+import com.guicai.cli.CommandExecutor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CommandExecutor commandExecutor = new CommandExecutor();
+        commandExecutor.doExecute(args);
+    }
+}
