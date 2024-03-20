22edc7db891a3686c23d07b54a892d419ec36ae3
guicaizhici <969774823@qq.com>
2024-03-20 13:27:27 +0800

    工作空间的隔离

--- guicai-generator-maker/src/main/java/com/guicai/maker/generator/file/FileGenerator.java
+++ guicai-generator-maker/src/main/java/com/guicai/maker/generator/file/FileGenerator.java
@@ -1,6 +1,5 @@
 package com.guicai.maker.generator.file;
 
-import com.guicai.maker.model.DataModel;
 import freemarker.template.TemplateException;
 
 import java.io.File;
@@ -35,11 +34,4 @@ public class FileGenerator {
         DynamicFileGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
     }
 
-    public static void main(String[] args) throws TemplateException, IOException {
-        DataModel dataModel = new DataModel();
-        dataModel.setAuthor("guicai");
-        dataModel.setLoop(false);
-        dataModel.setOutputText("求和结果：");
-        doGenerate(dataModel);
-    }
 }
---
+++ guicai-generator-maker/src/main/java/com/guicai/maker/template/TemplateMaker.java
@@ -0,0 +1,95 @@
+package com.guicai.maker.template;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.guicai.maker.meta.Meta;
+import com.guicai.maker.meta.enums.FileGenerateTypeEnum;
+import com.guicai.maker.meta.enums.FileTypeEnum;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author ${雷电影}
+ * @Version 1.0
+ */
+public class TemplateMaker {
+
+    public static void main(String[] args) {
+
+        // 指定原始项目路径
+        String projectPath = System.getProperty("user.dir");
+        String originProjectPath = new File(projectPath).getParent() + File.separator + "guicai-generator-demo-project/acm-template";
+
+        // 复制目录
+        long id = IdUtil.getSnowflakeNextId();
+        String tempDirPath = projectPath + File.separator + ".temp";
+        String templatePath = tempDirPath + File.separator + id;
+        if (!FileUtil.exist(templatePath)) {
+            FileUtil.mkdir(templatePath);
+        }
+        FileUtil.copy(originProjectPath, templatePath, true);
+
+        // 一、输入信息
+        // 1. 输入项目基本信息
+        String name = "acm-template-generator";
+        String description = "ACM 示例模板生成器";
+
+        // 2. 输入文件信息
+        String sourceRootPath = templatePath + File.separator + FileUtil.getLastPathEle(Paths.get(originProjectPath)).toString();
+        String fileInputPath = "src/com/guicai/acm/MainTemplate.java";
+        String fileOutputPath = fileInputPath + ".ftl";
+        // 注意 win 系统需要对路径进行转义
+        sourceRootPath = sourceRootPath.replaceAll("\\\\", "/");
+
+
+        // 3. 输入模型参数信息
+        Meta.ModelConfig.ModelInfo modelInfo = new Meta.ModelConfig.ModelInfo();
+        modelInfo.setFieldName("outputText");
+        modelInfo.setType("String");
+        modelInfo.setDefaultValue("sum = ");
+
+        // 二、使用字符串替换，生成模板文件
+        String fileInputAbsolutePath = sourceRootPath + File.separator + fileInputPath;
+        String fileContent = FileUtil.readUtf8String(fileInputAbsolutePath);
+        String replacement = String.format("${%s}", modelInfo.getFieldName());
+        String newFileContent = StrUtil.replace(fileContent, "Sum: ", replacement);
+
+        // 输出模板文件
+        String fileOutputAbsolutePath = sourceRootPath + File.separator + fileOutputPath;
+        FileUtil.writeUtf8String(newFileContent, fileOutputAbsolutePath);
+
+        // 三、生成配置文件
+        String metaOutputPath = sourceRootPath + File.separator + "meta.json";
+
+        // 1. 构造配置参数
+        Meta meta = new Meta();
+        meta.setName(name);
+        meta.setDescription(description);
+
+        Meta.FileConfig fileConfig = new Meta.FileConfig();
+        meta.setFileConfig(fileConfig);
+        fileConfig.setSourceRootPath(sourceRootPath);
+        List<Meta.FileConfig.FileInfo> fileInfoList = new ArrayList<>();
+        fileConfig.setFiles(fileInfoList);
+        Meta.FileConfig.FileInfo fileInfo = new Meta.FileConfig.FileInfo();
+        fileInfo.setInputPath(fileInputPath);
+        fileInfo.setOutputPath(fileOutputPath);
+        fileInfo.setType(FileTypeEnum.FILE.getValue());
+        fileInfo.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
+        fileInfoList.add(fileInfo);
+
+        Meta.ModelConfig modelConfig = new Meta.ModelConfig();
+        meta.setModelConfig(modelConfig);
+        List<Meta.ModelConfig.ModelInfo> modelInfoList = new ArrayList<>();
+        modelConfig.setModels(modelInfoList);
+        modelInfoList.add(modelInfo);
+
+        // 2. 输出元信息文件
+        FileUtil.writeUtf8String(JSONUtil.toJsonPrettyStr(meta), metaOutputPath);
+    }
+}
